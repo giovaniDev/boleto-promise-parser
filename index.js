508f990cda3b165eba68183ca5d1444a40ef8545
girvX <giovani.rdgs@gmail.com>
2019-08-19 10:31:17 -0300

    Index refatorado, todas as verif. sao feitas aqui

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const guia = require('./module/guia');
-const boleto = require('./module/boleto')
+const boleto = require('./module/boleto');
+const { verifyDigitoVerificadorGeralModule10Guia, verifyDigitoVerificadorGeralModule11Boleto } = require('./module/verifyers');
 
 const parser = (barcode) => {
-    if (barcode[0] === '8') {
-        return guia(barcode);
-    }
-    return boleto(barcode);
+    return new Promise((resolve, reject) => {
+        if (barcode === undefined) reject({error: 'É necessario colocar uma linha digitavel: 44 caracteres (string)'})
+        if (barcode.length !== 44) reject({error: 'São necessárias 44 caracteres'});
+        
+        if (barcode[0] === '8') {
+            const verifyinDigitoVerificador = verifyDigitoVerificadorGeralModule10Guia(barcode.split(''));
+            if (!verifyinDigitoVerificador) reject({error: 'Digito verificador geral inválido! Passe o scanner novamente'});
+            resolve(guia(barcode));
+        } else {
+            const verifyinDigitoVerificador = verifyDigitoVerificadorGeralModule11Boleto(barcode.split(''));
+            if (!verifyinDigitoVerificador) reject({error: 'Digito verificador geral inválido! Passe o scanner novamente'});
+            resolve(boleto(barcode));
+        }
+       
+    })
 }
 
 module.exports = parser;
